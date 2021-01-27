15601eadedf9a1f80bcd0b6c3c888ca7fce72e76
Greta Minkeviciute <gretaminkeviciute@yahoo.com>
2021-01-27 14:19:09 +0100

    Cow Spawning, Bull Spawning (adjustable), Score Implemented

--- Cowoclock/Assets/Scripts/CowController.cs
+++ Cowoclock/Assets/Scripts/CowController.cs
@@ -4,36 +4,110 @@ using UnityEngine;
 
 public class CowController : MonoBehaviour
 {
+    private enum  COWSPAWNSTATE{
+        WaitForSpawn,
+        Spawned
+    }  
+    
     [SerializeField] private Vector3[] positions_;
     [SerializeField] private int max_spawn_count_;
 
     [SerializeField] private float spawn_interval_;
-    [SerializeField] private float visible_suration_;
+    [SerializeField] private float visible_duration_;
+
+    [Range(0.0f, 1.0f)]
+    [SerializeField] private float horn_rate_;
 
     private float timer_;
 
-    private bool spawned_ = false;
+    private COWSPAWNSTATE state_ = COWSPAWNSTATE.WaitForSpawn;
+
+    [SerializeField] private GameObject cow_prefab_;
+    public List<GameObject> cows_ = new List<GameObject>();
+
+    public int cow_points_;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+        for (int i = 0; i < positions_.Length; i++)
+        {
+            cows_.Add(Instantiate(cow_prefab_, positions_[i], Quaternion.identity));
+        }
+
+        DespawnCows();
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer_ += timer_.deltaTime;
+        timer_ += Time.deltaTime;
 
-        if(timer_ > spawn_interval_ && !spawned_)
+        switch(state_)
         {
-            SpawnCows();
-            timer_ = 0.0f;
+            case COWSPAWNSTATE.WaitForSpawn:
+                if(timer_> spawn_interval_)
+                {
+                    SpawnCows();
+                    timer_ = 0.0f;
+                    state_ = COWSPAWNSTATE.Spawned;
+                }
+                break;
+            case COWSPAWNSTATE.Spawned:
+                if(timer_ > visible_duration_)
+                {
+                    DespawnCows();
+                    timer_ = 0.0f;
+                    state_ = COWSPAWNSTATE.WaitForSpawn;
+                }
+                break;
+            default:
+                Debug.Log("Why Tho");
+                break;
         }
+
+
     }
 
     private void SpawnCows()
     {
-        int spawn_count = Random.Range(0, max_spawn_count_ +1);
+        
+        int spawn_count = Random.Range(1, max_spawn_count_ +1);
+
+        List<int> indices = new List<int>();
+
+        // creating a list of indices (indexes) that represent the list of cows
+
+        for (int i = 0; i < cows_.Count; i++)
+        {
+            indices.Add(i);
+        }
+
+        // getting a random index and activate the cow in that index and then we remove that index
+
+        for (int i = 0; i < spawn_count; i++)
+        {
+            int random_index = Random.Range(0, indices.Count);
+
+            cows_[random_index].SetActive(true);
+
+            bool spawn_with_horns = (Random.value < horn_rate_);
+
+            cows_[random_index].transform.GetChild(0).gameObject.SetActive(spawn_with_horns);
+
+        // makes sure there aren't 2 in a row
+            indices.RemoveAt(random_index);
+        }
+
+    }
+
+    private void DespawnCows()
+    {
+        for (int i = 0; i < cows_.Count; i++)
+        {
+            cows_[i].SetActive(false);
+        }
     }
 }
--- Cowoclock/Assets/Scripts/PlayerController.cs
+++ Cowoclock/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,7 +11,14 @@ public class PlayerController : MonoBehaviour
     private int player_position_index_;
     private GameObject beam_;
     [SerializeField] private float beam_active_duration_;
-    
+
+    [SerializeField] private CowController cow_controller_;
+
+    private int points_ = 0;
+
+    public int hp_;
+
+    [SerializeField] private Text score_;
     
     // Start is called before the first frame update
     void Start()
@@ -30,18 +39,22 @@ public class PlayerController : MonoBehaviour
         {
             player_position_index_ -= 1;
             transform.position = positions_[player_position_index_];
+            beam_.SetActive(false);
         }
         if (Input.GetKeyDown(KeyCode.RightArrow) &&
             player_position_index_ < positions_.Length -1)
         {
             player_position_index_ += 1;
             transform.position = positions_[player_position_index_];
+            beam_.SetActive(false);
         }
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
             StopAllCoroutines();
             StartCoroutine(ActivateBeam());
+
+            CheckCow();
         }
     }
 
@@ -52,6 +65,27 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(beam_active_duration_);
 
         beam_.SetActive(false);
+    }
+
+    private void CheckCow()
+    {
+        if (!cow_controller_.cows_[player_position_index_].activeInHierarchy) return;
+
+        if (cow_controller_.cows_[player_position_index_].transform.GetChild(0).gameObject.activeInHierarchy)
+        {
+            //Damage
+            hp_--;
+
+            if (hp_ <= 0) SceneManager.LoadScene("Greta Scene");
+        }
+        else
+        {
+            //Points
+            points_ += cow_controller_.cow_points_;
+            score_.text = points_.ToString();
+        }
+
+        cow_controller_.cows_[player_position_index_].SetActive(false);
     }
 
 }
